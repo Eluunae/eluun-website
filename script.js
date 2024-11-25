0481046a41b4bec28c788ad0000c8f376e50d206
Eluunae <eluunsound@gmail.com>
2024-11-25 16:45:46 +0100

    maj dl 1

---
+++ script.js
@@ -0,0 +1,8 @@
+// scripts.js
+function openModal() {
+    document.getElementById('modal').style.display = 'block';
+}
+
+function closeModal() {
+    document.getElementById('modal').style.display = 'none';
+}
