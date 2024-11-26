3f7c137577daffa4208d370f2c5903f842c861d7
Eluunae <eluunsound@gmail.com>
2024-11-26 21:31:14 +0100

    maj dl 3, ajout de fonction API spotify

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 const clientId = '7d4fe1c6125c4a4b8d63d6f3b9f46ed7'; // Remplacez par votre client_id
 const redirectUri = 'https://eluun.link/download'; // Remplacez par votre URI de redirection
-const scopes = 'user-follow-modify user-library-modify'; // Scopes nécessaires
+const scopes = 'user-follow-modify user-library-modify user-follow-read user-library-read'; // Scopes nécessaires
 
 let currentUserId = '';
 let currentTrackId = '';
