b6e717974307ea44ae22a3d56641c9bf2b19dc6f
Eluunae <eluunsound@gmail.com>
2024-11-29 18:16:02 +0100

    test different discord presque fonctionnel 2.5

--- netlify/functions/discord-auth.js
+++ netlify/functions/discord-auth.js
@@ -1,6 +1,6 @@
 // netlify/functions/discord-auth.js
 exports.handler = async function() {
-  const authURL = "https://discord.com/oauth2/authorize?client_id=1311976301351014442&response_type=code&redirect_uri=https%3A%2F%2Feluun.link%2F.netlify%2Ffunctions%2Fdiscord-callback&scope=identify+guilds.join";
+  const authURL = "https://discord.com/oauth2/authorize?client_id=1311976301351014442&response_type=code&redirect_uri=https%3A%2F%2Feluun.link&scope=identify+guilds.join";
   
   return {
     statusCode: 302,
--- netlify/functions/process-discord.js
+++ netlify/functions/process-discord.js
@@ -1,35 +1,22 @@
 // netlify/functions/process-discord.js
-const fetch = require('node-fetch');
+const axios = require('axios');
 
 exports.handler = async function(event) {
   const { code } = JSON.parse(event.body);
-  
+
   try {
-    // 1. Échanger le code contre un token
-    const tokenResponse = await fetch('https://discord.com/api/oauth2/token', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
-      },
-      body: new URLSearchParams({
+    const tokenResponse = await axios.post('https://discord.com/api/oauth2/token',
+      new URLSearchParams({
         client_id: process.env.DISCORD_CLIENT_ID,
         client_secret: process.env.DISCORD_CLIENT_SECRET,
         code: code,
         grant_type: 'authorization_code',
         redirect_uri: 'https://eluun.link'
-      })
-    });
-
-    const tokenData = await tokenResponse.json();
-
-    // 2. Ajouter au serveur avec le rôle
-    await fetch(`https://discord.com/api/guilds/${process.env.DISCORD_GUILD_ID}/members/${tokenData.user.id}/roles/${process.env.DISCORD_ROLE_ID}`, {
-      method: 'PUT',
-      headers: {
-        Authorization: `Bot ${process.env.DISCORD_BOT_TOKEN}`,
-        'Content-Type': 'application/json'
+      }),
+      {
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
       }
-    });
+    );
 
     return {
       statusCode: 200,
