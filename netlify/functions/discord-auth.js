0881013576a69c46c047c257477e3f0367fe53ac
Eluunae <eluunsound@gmail.com>
2024-11-29 17:15:37 +0100

    test different discord 1

--- netlify/functions/discord-auth.js
+++ netlify/functions/discord-auth.js
@@ -1,14 +1,12 @@
 // netlify/functions/discord-auth.js
 exports.handler = async function(event, context) {
-  const DISCORD_CLIENT_ID = process.env.DISCORD_CLIENT_ID;
-  const DISCORD_REDIRECT_URI = process.env.DISCORD_REDIRECT_URI;
-
-  const authURL = `https://discord.com/api/oauth2/authorize?client_id=${DISCORD_CLIENT_ID}&redirect_uri=${encodeURIComponent(DISCORD_REDIRECT_URI)}&response_type=code&scope=identify%20guilds.join`;
-  
-  console.log('Auth URL:', authURL); // Debug
+  // Utiliser directement l'URL générée par Discord
+  const authURL = "https://discord.com/oauth2/authorize?client_id=1311976301351014442&response_type=code&redirect_uri=https%3A%2F%2Feluun.link%2F.netlify%2Ffunctions%2Fdiscord-callback&scope=identify+guilds.join";
 
   return {
-    statusCode: 200,
-    body: JSON.stringify({ url: authURL })
+    statusCode: 302,
+    headers: {
+      Location: authURL
+    }
   };
 };
