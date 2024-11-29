690aa8efa84b6924eae14a13f4e3bf66d370df01
Eluunae <eluunsound@gmail.com>
2024-11-29 10:05:42 +0100

    add discord

--- netifly/functions/addRole.js
+++ netifly/functions/addRole.js
@@ -2,34 +2,45 @@
 const { Client, Intents } = require('discord.js');
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS] });
-const token = process.env.MTMxMTk3NjMwMTM1MTAxNDQ0Mg.GlP4Sw.RhDs6nsFFLucxXu4wk_0j0A1fZJ5OvtpUZZ_UE;
-const guildId = process.env.1174313475552591933;
-const roleId = process.env.1311962320481812582;
+const token = process.env.DISCORD_BOT_TOKEN;
+const guildId = process.env.DISCORD_GUILD_ID;
+const roleId = process.env.DISCORD_ROLE_ID;
 
 client.login(token);
 
+client.once('ready', () => {
+  console.log('Bot is online!');
+});
+
 exports.handler = async (event, context) => {
+  console.log('Handler function called');
   const userId = event.queryStringParameters.userId;
+  console.log('User ID:', userId);
 
   try {
     const guild = await client.guilds.fetch(guildId);
+    console.log('Guild fetched:', guild.name);
     const member = await guild.members.fetch(userId);
+    console.log('Member fetched:', member.user.tag);
     const role = guild.roles.cache.get(roleId);
+    console.log('Role fetched:', role.name);
 
     if (member && role) {
       await member.roles.add(role);
+      console.log('Role assigned successfully');
       return {
         statusCode: 200,
         body: 'Role assigned successfully!',
       };
     } else {
+      console.log('Member or role not found');
       return {
         statusCode: 404,
         body: 'Member or role not found.',
       };
     }
   } catch (error) {
-    console.error(error);
+    console.error('Error occurred:', error);
     return {
       statusCode: 500,
       body: 'An error occurred.',
