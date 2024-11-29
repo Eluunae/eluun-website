0b0b0ce57b787ad2d2e2e1fbc65558240b4db6cc
Eluunae <eluunsound@gmail.com>
2024-11-29 16:36:00 +0100

    add discord OAuth2 4

--- netlify/functions/getConfig.js
+++ netlify/functions/getConfig.js
@@ -1,20 +1,18 @@
 // netlify/functions/getConfig.js
 exports.handler = async function() {
-  if (!process.env.DISCORD_CLIENT_ID) {
-    console.error('DISCORD_CLIENT_ID not set');
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: 'Configuration error' })
-    };
-  }
-
+  console.log('getConfig called');
   try {
+    if (!process.env.DISCORD_CLIENT_ID) {
+      throw new Error('Missing DISCORD_CLIENT_ID');
+    }
+    
     return {
       statusCode: 200,
       headers: {
         'Content-Type': 'application/json',
         'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'GET'
+        'Access-Control-Allow-Methods': 'GET',
+        'Cache-Control': 'no-cache'
       },
       body: JSON.stringify({
         clientId: process.env.DISCORD_CLIENT_ID,
@@ -22,6 +20,7 @@ exports.handler = async function() {
       })
     };
   } catch (error) {
+    console.error('getConfig error:', error);
     return {
       statusCode: 500,
       body: JSON.stringify({ error: error.message })
