5e6622d2306210ecbfe0ec02a3d3a7ac36ab41e7
Eluunae <eluunsound@gmail.com>
2024-11-29 15:17:04 +0100

    add discord OAuth2 4

--- netlify/functions/getConfig.js
+++ netlify/functions/getConfig.js
@@ -1,13 +1,31 @@
 // netlify/functions/getConfig.js
 exports.handler = async function() {
-  return {
-    statusCode: 200,
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      clientId: process.env.DISCORD_CLIENT_ID,
-      redirectUri: 'https://eluun.link/callback' // URL fixe ou process.env.REDIRECT_URI
-    })
-  };
+  if (!process.env.DISCORD_CLIENT_ID) {
+    console.error('DISCORD_CLIENT_ID not set');
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Configuration error' })
+    };
+  }
+
+  try {
+    return {
+      statusCode: 200,
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'Cache-Control': 'no-store'
+      },
+      body: JSON.stringify({
+        clientId: process.env.DISCORD_CLIENT_ID,
+        redirectUri: 'https://eluun.link/callback',
+        scope: 'identify guilds.join'
+      })
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: error.message })
+    };
+  }
 };
